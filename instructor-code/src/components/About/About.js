4e5b99936a78bb2867c5be7f07b0e7007be62242
Paul Quiroz <pquiroz1977@gmail.com>
2018-05-15 12:36:51 -0700

    initial commit

--- instructor-code/src/components/About/About.js
+++ instructor-code/src/components/About/About.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 
-
 export default function About(props) {
   return (
     <div>
       <h1>This is the About component</h1>
-      <Link to='/about/faq'><h3>FAQ</h3></Link>
-      <Link to='/about/company'><h3>Company</h3></Link>
+      <Link to="/about/faq"><h3>FAQ</h3></Link>
+      <Link to="/about/company"><h3>Company</h3></Link>
       {props.children}
-     
-      
     </div>
   )
 }
--- instructor-code/src/components/App.js
+++ instructor-code/src/components/App.js
@@ -4,8 +4,8 @@ import About from './About/About';
 import Home from './Home/Home';
 import People from './People/People';
 import FAQ from './About/FAQ/FAQ';
-import Company from './About/Company/Company'
-import Character from './Character/Character'
+import Company from './About/Company/Company';
+import Character from './Character/Character';
 
 class App extends Component {
   render() {
@@ -13,23 +13,22 @@ class App extends Component {
       <div>
         This is App.js
         <nav>
-          <Link to='/'>Home</Link>
-          <Link to ='/about'>About</Link>
-          <Link to ='/people'>People</Link>
-
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/people">People</Link>
         </nav>
         <main>
           <Route exact path="/" component={Home} />
           <Route path="/about" render={() => {
-            return(
+            return (
               <About>
-                <Route path='/about/faq' component={FAQ}/>
-                <Route path='/about/company' component={Company}/>
+                <Route path="/about/faq" component={FAQ} />
+                <Route path="/about/company" component={Company} />
               </About>
             )
-          }}/>
+          }} />
           <Route exact path="/people" component={People} />
-          <Route path='/people/:id' component={Character} />
+          <Route path="/people/:id" component={Character} />
         </main>
       </div>
     );
